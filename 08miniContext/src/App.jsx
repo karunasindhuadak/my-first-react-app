46b7e2493cd0a4a09db903635d24980709fdd08b
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2025-10-28 01:20:30 +0530

    mini project complete

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h2>Hello My name is Karunasindhu Adak</h2>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
