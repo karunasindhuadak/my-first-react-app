9874bed4c982067ef17c4fb1e4e8e03ef24528e0
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2025-12-29 12:10:24 +0530

    update

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>I am learning ReduxToolkit first time</h1>
+    </>
+  )
+}
+
+export default App
