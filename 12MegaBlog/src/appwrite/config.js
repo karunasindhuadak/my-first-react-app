82a6c2880d57b30a2e2ca363a33a39eba70df804
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2026-01-08 21:47:35 +0530

    appwrite mostly done

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,135 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client()
+    databases
+    bucket
+
+    constructor () {
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument({
+                databaseId: conf.appwriteDatabaseId,
+                collectionId: conf.appwriteCollectionId,
+                documentId: slug,
+                data: {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            })
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error)
+        }
+    }
+
+    async updatePost (slug, {title, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument({
+                databaseId: conf.appwriteDatabaseId,
+                collectionId: conf.appwriteCollectionId,
+                documentId: slug,
+                data: {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            })
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error)
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument({
+                databaseId: conf.appwriteDatabaseId,
+                collectionId: conf.appwriteCollectionId,
+                documentId: slug
+            })
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error)
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return this.databases.getDocument({
+                databaseId: conf.appwriteDatabaseId,
+                collectionId: conf.appwriteCollectionId,
+                documentId: slug
+            })
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error)
+            return false
+        }
+    }
+
+    async getPosts(){
+        try {
+            return await this.databases.listDocuments({
+                databaseId: conf.appwriteDatabaseId,
+                collectionId: conf.appwriteCollectionId,
+                queries: [
+                    Query.equal("status", "active")
+                ]
+            })
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error)
+            return false
+        }
+    }
+
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile({
+                bucketId: conf.appwriteBucketId,
+                fileId: ID.unique(),
+                file: file
+            })
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error)
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile({
+                bucketId: conf.appwriteBucketId,
+                fileId: fileId
+            })
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview({
+            bucketId: conf.appwriteBucketId,
+            fileId: fileId
+        })
+    }
+}
+
+const service = new Service()
+
+export default service
