f0aebc43fd98faa9da6547e050015bc5614e2c6d
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2026-01-14 23:54:11 +0530

    Postform done

---
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,79 @@
+import React, { use, useCallback, useEffect } from "react";
+import RTE from "../components/RTE";
+import { useForm } from "react-hook-form";
+import service from "../../appwrite/config";
+import { useSelector } from "react-redux";
+import { data, useNavigate } from "react-router-dom";
+
+function PostForm({ post }) {
+  const { register, handleSubmit, control, watch, setValue, getValues } = useForm({
+    defaultValues: {
+      title: post?.title || "",
+      slug: post?.$id || "",
+      content: post?.content || "",
+      status: post?.status || "active",
+    },
+  });
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.userData.user);
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? await service.uploadFile(data.image[0])
+        : null;
+
+      if (file) {
+        await service.deleteFile(post.featuredImage);
+      }
+
+      const dbPost = await service.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+
+      if (dbPost) {
+        navigate(`/post/${dbPost.$id}`);
+      }
+    } else {
+      const file = data.image[0]
+        ? await service.uploadFile(data.image[0])
+        : null;
+      if (file) {
+        const fileId = file.$id;
+        data.featuredImage = fileId;
+        const dbPost = await service.createPost({
+          ...data,
+          userId: userData.$id,
+        });
+
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }
+      }
+    }
+  };
+
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-zA-Z\d\s]+/g, "-")
+        .replace(/\s/g, "-");
+    }
+    return "";
+  }, []);
+
+  useEffect(() => {
+    const subcription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title), { shouldValidate: true });
+      }
+    });
+    return () => subcription.unsubscribe();
+  }, [watch, setValue, slugTransform]);
+  return <div></div>;
+}
+
+export default PostForm;
